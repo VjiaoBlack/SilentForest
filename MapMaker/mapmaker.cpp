308d7241213c526857d00b01b412653704d96a32
Victor Jiao <VjiaoBlack@gmail.com>
2014-03-09 20:10:58 -0400

    mapdata data parsing seems to be pretty much finished. None of it is actually yet functional because ids as of yet have no meaning; currently polishing / working on output

--- MapMaker/mapmaker.cpp
+++ MapMaker/mapmaker.cpp
@@ -1,4 +1,4 @@
-// #import "SDL/SDL.h"
+#import "SDL/SDL.h"
 #import "id.h"
 #import <stdio.h>
 #import <stdlib.h>
@@ -166,26 +166,26 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// void init() {
-//     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-//         printf("Could not initialize SDL: %s\n", SDL_GetError());
-//         exit(1);
-//     }
+void init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
     
-//     /* Open a 640 x 480 screen */
-//     screen = SDL_SetVideoMode( 640, 480, 0, SDL_HWSURFACE | SDL_DOUBLEBUF );
-//     if (screen == NULL) {
-//         printf("Couldn't set screen mode to 640 x 480: %s\n", SDL_GetError());
-//         exit(1);
-//     }
+    /* Open a 640 x 480 screen */
+    screen = SDL_SetVideoMode( 640, 480, 0, SDL_HWSURFACE | SDL_DOUBLEBUF );
+    if (screen == NULL) {
+        printf("Couldn't set screen mode to 640 x 480: %s\n", SDL_GetError());
+        exit(1);
+    }
     
-//     /* Set the screen title */ // ???
-//     SDL_WM_SetCaption("Silent Forest Map Editor", "Editor");
-// }
+    /* Set the screen title */ // ???
+    SDL_WM_SetCaption("Silent Forest Map Editor", "Editor");
+}
 
-// Tile* parse_tile(char* tiledata) {
-//     return NULL;
-// }
+Tile* parse_tile(char* tiledata) {
+    return NULL;
+}
 
 void init_map(int id) {
     int x, y;
